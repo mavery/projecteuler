9925ee045f5a275741dc55c6890a3b66a55c7e30
Michael Avery <mavery@gmail.com>
2011-06-02 22:27:01 +1000

    Adding solution to problem 92

---
+++ src/mavery/projecteuler/Problem92.java
@@ -0,0 +1,71 @@
+package mavery.projecteuler;
+
+public class Problem92
+{
+
+	public static final int LIMIT = 10000000;
+	
+	private static int[] storedResults;
+
+	/**
+	 * A number chain is created by continuously adding the square of the digits
+	 * in a number to form a new number until it has been seen before.
+	 * 
+	 * For example,
+	 * 
+	 * 44 32 13 10 1 1 
+	 * 
+	 * 85 89 145 42 20 4 16 37 58 89
+	 * 
+	 * Therefore any chain that arrives at 1 or 89 will become stuck in an
+	 * endless loop. What is most amazing is that EVERY starting number will
+	 * eventually arrive at 1 or 89.
+	 * 
+	 * How many starting numbers below ten million will arrive at 89?
+	 */
+	public static void main(String[] args)
+	{
+		storedResults = new int[LIMIT];
+		storedResults[0] = -1;
+		storedResults[88] = 1;
+		
+		int result = 0;
+		for (int i = 1; i < LIMIT; i++)
+		{
+			if (chainReaches89(i))
+			{
+				result++;		
+			}
+		}
+
+		System.out.println(result);
+	}
+	
+	/**
+	 * Returns true if the number chain eventually reaches 89
+	 * @param n Starting number in the chain
+	 * @return True if the number chain reaches 89, false if it reaches 1
+	 */
+	public static boolean chainReaches89(int n)
+	{
+		int i = n;
+		while (true)
+		{
+			if (storedResults[i - 1] != 0)
+			{
+				storedResults[n - 1] = storedResults[i - 1];
+				return storedResults[n - 1] == 1;
+			}
+
+			int temp = 0;
+			while (i != 0)
+			{
+				int cur = i % 10;
+				temp += (cur * cur);
+				i /= 10;
+			}
+			i = temp;
+		}
+	}
+
+}
