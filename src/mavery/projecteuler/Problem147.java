c1111618993c4806f0b200c66fdfa113f20e58c2
Michael Avery <mavery@gmail.com>
2011-06-02 19:26:04 +1000

    Significant improvements and cleanup to solution for problem 147.

--- src/mavery/projecteuler/Problem147.java
+++ src/mavery/projecteuler/Problem147.java
@@ -25,7 +25,7 @@ public class Problem147
 	 * grids?
 	 */
 	public static void main(String[] args)
-	{
+	{	
 		int result = 0;
 
 		for (int i = 1; i <= MAX_X; i++)
@@ -33,23 +33,19 @@ public class Problem147
 			for (int j = 1; j <= MAX_Y; j++)
 			{
 				result += countRectangles(i, j);
-				result += countDiamonds(i, j);
-
-				// System.out.printf("%d %d : %d\n", i, j, countRectangles(i, j)
-				// + countDiamonds(i, j));
+				result += countCrossHatch(i, j);
 			}
 		}
 
 		System.out.println(result);
 	}
 
+	/**
+	 * Returns the number of rectangles that can be created using the horizontal
+	 * and vertical grid lines
+	 */
 	public static int countRectangles(int x, int y)
 	{
-		if (x <= 0 || y <= 0)
-		{
-			return 0;
-		}
-
 		int result = 0;
 		for (int i = 1; i <= x; i++)
 		{
@@ -62,51 +58,36 @@ public class Problem147
 		return result;
 	}
 
-	public static int countDiamonds(int x, int y)
+	/**
+	 * Returns the total number of rectangles that can be created using the cross-hatched lines. 
+	 */
+	public static int countCrossHatch(int x, int y)
 	{
-		if (x > y)
-		{
-			int temp = x;
-			x = y;
-			y = temp;
-		}
 
-		if (x <= 0)
-		{
-			return 0;
-		}
-
-		if (x == 1)
-		{
-			return Math.max(0, y - 1);
-		}
-
-		int basicDiamonds = 2 * x * (x - 1) + ((2 * x) - 1) * (y - x);
+		// Initially this is the number of 1x1 squares
+		int squares = 2 * x * (x - 1) + ((2 * x) - 1) * (y - x);
 		int result = 0;
 
-		for (int i = 1; i < 2 * x; i++)
+		for (int rowCount = 0, nextRow = (x + y - 2); squares > 0; rowCount++)
 		{
-			for (int j = 1; j < 2 * y; j++)
+			int current = squares;
+			for (int colCount = 0, nextCol = (x + y - 2) - rowCount; current > 0; colCount++)
 			{
-				int current = basicDiamonds;
+				result += current;
 
-				int rowsRemoved = 0;
-				for (int a = (x + y - 2), b = j - 1, c = 0; a > 0 && b > 0; c ^= 1, b--, a = c == 1 ? a
-						: a - 2)
+				// Adds one to the x dimension of the rectangle
+				current -= nextCol;
+				if (colCount % 2 == 1)
 				{
-					current -= a;
-					rowsRemoved++;
+					nextCol -= 2;
 				}
-				for (int a = (x + y - 2) - rowsRemoved, b = i - 1, c = 0; a > 0
-						&& b > 0; c ^= 1, b--, a = c == 1 ? a : a - 2)
-				{
-					current -= a;
-				}
+			}
 
-				if (current > 0)
-				{
-					result += current;
-				}
+			// Adds one to the y dimension of the rectangle
+			squares -= nextRow;
+			if (rowCount % 2 == 1)
+			{
+				nextRow -= 2;
 			}
 		}
 
