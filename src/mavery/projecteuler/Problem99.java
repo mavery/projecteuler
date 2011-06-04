bc8ddaeae96204cccf2a1aed5e8e0c6e0895597f
Michael Avery <mavery@gmail.com>
2011-06-04 14:00:54 +1000

    Cleaning up description of problem 99

--- src/mavery/projecteuler/Problem99.java
+++ src/mavery/projecteuler/Problem99.java
@@ -9,8 +9,8 @@ public class Problem99
 {
 
 	/**
-	 * Comparing two numbers written in index form like 211 and 37 is not
-	 * difficult, as any calculator would confirm that 211 = 2048 37 = 2187.
+	 * Comparing two numbers written in index form like 2^11 and 3^7 is not
+	 * difficult, as any calculator would confirm that 2^11 = 2048 3^7 = 2187.
 	 * 
 	 * However, confirming that 632382^518061 519432^525806 would be much more
 	 * difficult, as both numbers contain over three million digits.
