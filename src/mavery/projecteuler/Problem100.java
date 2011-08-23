563bbea822a0876bdf1e540f0f789e12132e1ed9
Michael Avery <mavery@gmail.com>
2011-08-24 00:04:15 +1000

    Working solution for problem 100. Will refactor this to not use BigDecimal.

--- src/mavery/projecteuler/Problem100.java
+++ src/mavery/projecteuler/Problem100.java
@@ -1,10 +1,15 @@
 package mavery.projecteuler;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Problem100
 {
-	public static final long MINIMUM = 100_000_000_000L;
+	public static final BigDecimal MINIMUM = BigDecimal.valueOf(1_000_000_000_000L);
+
+	private static final BigDecimal ROOT_2 = BigDecimal.valueOf(Math.sqrt(2.0));
 
 	/**
 	 * If a box contains twenty-one coloured discs, composed of fifteen blue
@@ -22,20 +27,38 @@ public class Problem100
 	 */
 	public static void main(String[] args)
 	{
-		double root2 = Math.sqrt(2);
-		
-		for (long sum = MINIMUM; ; sum++)
+		for (int i = 1; true; i++)
 		{
-			BigInteger blue = BigInteger.valueOf(((long) (sum / root2)) + 1);
-			BigInteger sumBig = BigInteger.valueOf(sum);
-		
-			BigInteger numerator = blue.multiply(blue.subtract(BigInteger.ONE));
-			BigInteger denominator = sumBig.multiply(sumBig.subtract(BigInteger.ONE));
-			
-			if (numerator.shiftLeft(1).equals(denominator))
+			BigDecimal b = b(i);
+			BigDecimal r = r(i);
+			System.out.printf("Blue = %s, Red = %s\n", b, r);
+			if (b.add(r).compareTo(MINIMUM) > 0)
 			{
-				System.out.println(blue);
+				return;
 			}
 		}
+	}
+
+	public static BigDecimal b(int n)
+	{
+		BigDecimal result = BigDecimal.ZERO;
+		result = result.add(BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(3).subtract(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.add(ROOT_2.multiply(BigDecimal.valueOf(3).subtract(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.add(BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(3).add(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.subtract(ROOT_2.multiply(BigDecimal.valueOf(3).add(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.add(BigDecimal.valueOf(4));
+		result = result.divide(BigDecimal.valueOf(8));
+		return result.setScale(0, RoundingMode.HALF_UP);
+	}
+
+	public static BigDecimal r(int n)
+	{
+		BigDecimal result = BigDecimal.ZERO;
+		result = result.subtract(BigDecimal.valueOf(4).multiply(BigDecimal.valueOf(3).subtract(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.subtract(BigDecimal.valueOf(3).multiply(ROOT_2).multiply(BigDecimal.valueOf(3).subtract(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.subtract(BigDecimal.valueOf(4).multiply(BigDecimal.valueOf(3).add(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.add(BigDecimal.valueOf(3).multiply(ROOT_2).multiply(BigDecimal.valueOf(3).add(BigDecimal.valueOf(2).multiply(ROOT_2)).pow(n)));
+		result = result.divide(BigDecimal.valueOf(8));
+		return result.setScale(0, RoundingMode.HALF_UP);
 	}
 }
