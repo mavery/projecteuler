5c350bf361764fee17a800da4a1c27df6fda783c
Michael Avery <mavery@gmail.com>
2011-08-24 15:26:21 +1000

    Rewrote solution to problem 78 to use the generating function for the partition function .

--- src/mavery/projecteuler/Problem78.java
+++ src/mavery/projecteuler/Problem78.java
@@ -2,10 +2,10 @@ package mavery.projecteuler;
 
 public class Problem78
 {
-	private static final int LIMIT = 10000;
-	private static int[][] PRECALC = new int[LIMIT + 1][LIMIT + 1];
-
 	public static final int MOD = 1000000;
+
+	private static final int LIMIT = 100000;
+
 	/**
 	 * Let p(n) represent the number of different ways in which n coins can be
 	 * separated into piles. For example, five coins can separated into piles in
@@ -18,47 +18,45 @@ public class Problem78
 	 */
 	public static void main(String[] args)
 	{
-		for (int i = 1; i <= LIMIT; i++)
+		int[] results = new int[LIMIT + 1];
+		results[0] = 1;
+		results[1] = 1;
+
+		// using the generating function from
+		// http://en.wikipedia.org/wiki/Partition_(number_theory)#Generating_function
+		for (int n = 2; n <= LIMIT; n++)
 		{
-			if (p_mod(i) == 0)
+			int result = 0;
+			for (int i = 1; true; i++)
 			{
-				System.out.printf("p(%d) mod %d = %d\n", i, MOD, p_mod(i));
+				// calculating the pentagonal number we need.
+				int x = (i % 2 == 0 ? -(i / 2) : (i / 2) + 1);
+				int pent = x * (3 * x - 1) / 2;
+
+				if (pent > n)
+				{
+					break;
+				}
+
+				// calculating whether to add or subtract this number
+				result = (((i - 1) & 2) == 0 ? result + results[n - pent]
+						: result - results[n - pent]);
+				if (result >= MOD)
+				{
+					result -= MOD;
+				}
+				else if (result < 0)
+				{
+					result += MOD;
+				}
+			}
+
+			results[n] = result;
+			if (result == 0)
+			{
+				System.out.printf("p(%d) mod %d = %d\n", n, MOD, result);
+				return;
 			}
 		}
-
-	}
-
-	public static int p_mod(int n)
-	{
-		return p_mod(n, n);
-	}
-
-	private static int p_mod(int n, int maxPile)
-	{
-		if (maxPile > n)
-		{
-			maxPile = n;
-		}
-		if (PRECALC[n][maxPile] != 0)
-		{
-			return PRECALC[n][maxPile];
-		}
-		if (n <= 1)
-		{	
-			return 1;
-		}
-		
-		int result = 0;
-		for (int i = maxPile; i > 0; i--)
-		{
-			result += p_mod(n - i, i);
-			if (result >= MOD)
-			{
-				result %= MOD;
-			}
-		}
-		
-		PRECALC[n][maxPile] = result;
-		return result;
 	}
 }
